ca85ad63f7a75d0b451f7ed80f6c13fb44825ee2
Maslomeister <maslomeister@gmail.com>
2022-04-14 13:59:14 +0300

    fix: Конфиги

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,10 @@
-declare module "*.module.css";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "*.module.css" {
+	const content: any;
+	export default content;
+}
+
+declare module "*.svg" {
+	const content: any;
+	export default content;
+}
