20dffc9823015ff9fd68164c8980e6fa14cb19bb
Maslomeister <maslomeister@gmail.com>
2022-05-11 16:21:59 +0300

    feat: Банер

--- src/new-components/banner/banner.tsx
+++ src/new-components/banner/banner.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+import Button from "../button/button";
+import { AvatarIcon } from "../../assets/icons";
 
 import styles from "./banner.module.css";
 
@@ -22,59 +26,138 @@ type TProfile = {
 
 type TBannerInfo = {
 	appName: string;
-	token: string;
 };
 
 type TTextBanner = {
-	type?: "text";
+	type: "text";
+	appName: string;
 	onFollow?: never;
 	onUnfollow?: never;
 	onEditArticle?: never;
 	onRemoveArticle?: never;
-	profile?: never;
+	isUser?: never;
+	user?: never;
 	article?: never;
 };
 
 type TProfileBanner = {
-	type?: "profile";
-	onFollow?: () => void;
-	onUnfollow?: () => void;
+	type: "profile";
+	appName?: never;
+	onFollow: () => void;
+	onUnfollow: () => void;
 	onEditArticle?: never;
 	onRemoveArticle?: never;
-	profile?: TProfile;
+	isUser: boolean;
+	user: TProfile;
 	article?: never;
 };
 
 type TArticleBanner = {
-	type?: "article";
+	type: "article";
+	appName?: never;
 	onFollow?: never;
 	onUnfollow?: never;
-	onEditArticle?: () => void;
-	onRemoveArticle?: () => void;
-	profile?: never;
-	article?: TArticle;
+	onEditArticle: () => void;
+	onRemoveArticle: () => void;
+	isUser: boolean;
+	user?: never;
+	article: TArticle;
 };
 
-type TBanner = TBannerInfo & (TTextBanner | TProfileBanner | TArticleBanner);
+type TBanner = TTextBanner | TProfileBanner | TArticleBanner;
 
-function ProfileBanner({ onFollow, onUnfollow }: TProfileBanner) {
+function EditUserProfile({ isUser }: { isUser: boolean }) {
+	const [redirect, setRedirect] = useState(false);
+	if (!isUser) {
+		return null;
+	}
+
 	return (
-		<div className={styles["banner_text"]}>
-			<h1 className={`${styles["banner_shadow"]} text text_type_main-h1`}>
-				Проектная кухня
-			</h1>
-			<h3 className={`${styles["banner_shadow"]} text text_type_main-headline`}>
-				Место, где готовится новый опыт
-			</h3>
+		<>
+			<Button
+				type="button"
+				onClick={() => {
+					setRedirect(true);
+				}}
+				iconType="edit"
+			>
+				Редактировать профиль
+			</Button>
+			{redirect && <Redirect to="/settings" push={true} />}
+		</>
+	);
+}
+
+function FollowUserButton({
+	isUser,
+	user,
+	onFollow,
+	onUnfollow,
+}: {
+	isUser: boolean;
+	user: TProfile;
+	onFollow: (username: string) => void;
+	onUnfollow: (username: string) => void;
+}) {
+	const handleClick = () => {
+		if (user?.following) {
+			onUnfollow(user.username);
+		} else {
+			onFollow(user.username);
+		}
+	};
+
+	if (isUser) {
+		return null;
+	}
+
+	return (
+		<Button
+			type="button"
+			onClick={handleClick}
+			iconType={user.following ? "unfollow" : "follow"}
+		>
+			{user.following ? "Unfollow" : "Follow"}
+		</Button>
+	);
+}
+
+function ProfileBanner({
+	isUser,
+	user,
+	onFollow,
+	onUnfollow,
+}: Omit<TProfileBanner, "type">) {
+	return (
+		<div className={styles["banner_profile"]}>
+			<div className={styles["banner_profile_inner"]}>
+				<AvatarIcon width={120} height={120} />
+				<h3
+					className={`${styles["banner_profile__username"]} text text_type_main-headline`}
+				>
+					{user.username}
+				</h3>
+
+				<div className=""></div>
+				<div className={styles["banner_profile_buttons"]}>
+					<EditUserProfile isUser={isUser} />
+					<FollowUserButton
+						isUser={isUser}
+						user={user}
+						onFollow={onFollow}
+						onUnfollow={onUnfollow}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-function TextBanner() {
+function TextBanner({ appName }: TBannerInfo) {
 	return (
 		<div className={styles["banner_text"]}>
 			<h1 className={`${styles["banner_shadow"]} text text_type_main-h1`}>
-				Проектная кухня
+				{appName}
 			</h1>
 			<h3 className={`${styles["banner_shadow"]} text text_type_main-headline`}>
 				Место, где готовится новый опыт
@@ -84,7 +167,11 @@ function TextBanner() {
 }
 
 export function Banner({
+	appName,
 	type = "text",
+	isUser,
+	user,
+	article,
 	onFollow,
 	onUnfollow,
 	onEditArticle,
@@ -92,11 +179,23 @@ export function Banner({
 }: TBanner) {
 	let content;
 	switch (type) {
+		case "article":
+			break;
 		case "profile":
-			content = <ProfileBanner onFollow={onFollow} />;
+			content = (
+				<ProfileBanner
+					isUser={isUser!}
+					user={user!}
+					onFollow={onFollow!}
+					onUnfollow={onUnfollow!}
+				/>
+			);
+			break;
+		case "text":
+			content = <TextBanner appName={appName!} />;
 			break;
 		default:
-			content = <TextBanner />;
+			content = <TextBanner appName={appName!} />;
 			break;
 	}
 	return (
