14aa9bdf79c52c254a0f872643f9f6fa64d8471d
Maslomeister <maslomeister@gmail.com>
2022-05-12 01:33:37 +0300

    refactor: Улучшил типизацию

--- src/new-components/banner/banner.tsx
+++ src/new-components/banner/banner.tsx
@@ -11,64 +11,42 @@ export type TBannerInfo = {
 export type TTextBanner = {
 	type: "text";
 	appName: string;
-	onFollow?: never;
-	onUnfollow?: never;
-	isUser?: never;
-	user?: never;
-	article?: never;
 };
 
 export type TProfileBanner = {
 	type: "profile";
-	appName?: never;
 	onFollow: () => void;
 	onUnfollow: () => void;
 	isUser: boolean;
 	user: TUser;
-	article?: never;
 };
 
 export type TArticleBanner = {
 	type: "article";
-	appName?: never;
-	onFollow?: never;
-	onUnfollow?: never;
 	isUser: boolean;
-	user?: never;
 	article: TArticle;
 };
 
 type TBanner = TTextBanner | TProfileBanner | TArticleBanner;
 
-export function Banner({
-	appName,
-	type = "text",
-	isUser,
-	user,
-	article,
-	onFollow,
-	onUnfollow,
-}: TBanner) {
+export const Banner: React.FC<TBanner> = (props) => {
 	let content;
-	switch (type) {
+	switch (props.type) {
 		case "article":
-			content = <ArticleBanner isUser={isUser!} article={article!} />;
+			content = <ArticleBanner isUser={props.isUser} article={props.article} />;
 			break;
 		case "profile":
 			content = (
 				<ProfileBanner
-					isUser={isUser!}
-					user={user!}
-					onFollow={onFollow!}
-					onUnfollow={onUnfollow!}
+					isUser={props.isUser}
+					user={props.user}
+					onFollow={props.onFollow}
+					onUnfollow={props.onUnfollow}
 				/>
 			);
 			break;
 		case "text":
-			content = <AppBanner appName={appName!} />;
-			break;
-		default:
-			content = <AppBanner appName={appName!} />;
+			content = <AppBanner appName={props.appName} />;
 			break;
 	}
 	return (
@@ -76,4 +54,4 @@ export function Banner({
 			<nav className={styles["banner"]}>{content}</nav>
 		</div>
 	);
-}
+};
