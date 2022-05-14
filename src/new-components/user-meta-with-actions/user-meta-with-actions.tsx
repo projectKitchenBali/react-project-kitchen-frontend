c8284ea8889c6974073792b890d173286c5c44e4
Maslomeister <maslomeister@gmail.com>
2022-05-14 19:08:08 +0300

    refactor: Мелкие улучшения

--- src/new-components/user-meta-with-actions/user-meta-with-actions.tsx
+++ src/new-components/user-meta-with-actions/user-meta-with-actions.tsx
@@ -28,41 +28,42 @@ export type TArticleActions = {
 type TUserMetaWithActions = TArticleActions;
 
 export const UserMetaWithActions: React.FC<TUserMetaWithActions> = (props) => {
-	let articleActions;
+	let content;
 	switch (props.actionsType) {
 		case "article":
-			articleActions = (
-				<ArticleActions canModify={props.canModify} article={props.article} />
+			content = (
+				<div className={`${styles["user-meta"]} text_type_main-default`}>
+					<Link
+						className={styles["avatar"]}
+						to={`/@${props.article.author.username}`}
+					>
+						<AvatarIcon width={40} height={40} />
+					</Link>
+
+					<div className={styles["info"]}>
+						<Link
+							className={styles["author"]}
+							to={`/@${props.article.author.username}`}
+						>
+							{props.article.author.username}
+						</Link>
+						<span className={`${styles["date"]} text_type_main-caption`}>
+							{new Date(props.article.createdAt)
+								.toLocaleDateString("ru-RU", options)
+								.replace(/\s*г\./, "")}
+						</span>
+					</div>
+					<div className={styles["actions"]}>
+						<ArticleActions
+							canModify={props.canModify}
+							article={props.article}
+						/>
+					</div>
+				</div>
 			);
-			break;
-		default:
-			articleActions = null;
+
 			break;
 	}
 
-	return (
-		<div className={`${styles["user-meta"]} text_type_main-default`}>
-			<Link
-				className={styles["avatar"]}
-				to={`/@${props.article.author.username}`}
-			>
-				<AvatarIcon width={40} height={40} />
-			</Link>
-
-			<div className={styles["info"]}>
-				<Link
-					className={styles["author"]}
-					to={`/@${props.article.author.username}`}
-				>
-					{props.article.author.username}
-				</Link>
-				<span className={`${styles["date"]} text_type_main-caption`}>
-					{new Date(props.article.createdAt)
-						.toLocaleDateString("ru-RU", options)
-						.replace(/\s*г\./, "")}
-				</span>
-			</div>
-			<div className={styles["actions"]}>{articleActions}</div>
-		</div>
-	);
+	return <>{content}</>;
 };
