0ebd7aeb6cf9147abd580fbc653471684ebe1044
Alexander Tarasov <tarassov.al@gmail.com>
2022-06-14 00:46:47 +0300

    fix: вернул localhost api

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,8 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = "http://kitchen.bonuts.ru/api";
+//const API_ROOT = "http://kitchen.bonuts.ru/api";
+const API_ROOT = "http://localhost:3000/api";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
